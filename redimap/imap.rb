22f125127199c3e7bb01cc5fc2472fe7d6c8e672
tiredpixel <tp@tiredpixel.com>
2013-04-10 20:03:19 +0100

    Implement Redimap::IMAP#read_mailbox.
    This reads a mailbox for any uids past some point.

--- redimap/imap.rb
+++ redimap/imap.rb
@@ -31,5 +31,13 @@ module Redimap
       end
     end
     
+    def read_mailbox(mailbox = "INBOX", last_seen_uid = 0)
+      @imap.select(mailbox)
+      
+      uids = @imap.uid_search("#{last_seen_uid + 1}:*")
+      
+      uids.find_all { |uid| uid > last_seen_uid } # IMAP search gets fun with edge cases.
+    end
+    
   end
 end
