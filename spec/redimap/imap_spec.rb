db6a2e52cfc7ea00bb3d6a4ff72d114d2029f410
tiredpixel <tp@tiredpixel.com>
2013-04-10 19:36:01 +0100

    Refactor Redimap::IMAP tests, removing pending invalid connection.
    Invalid connection tests will not currently be handled; the pending
    markers are removed, to make the testing clearer.

--- spec/redimap/imap_spec.rb
+++ spec/redimap/imap_spec.rb
@@ -4,65 +4,45 @@ require 'spec_helper'
 
 describe Redimap::IMAP do
   
+  before(:each) do
+    @fake_net_imap = double(Net::IMAP)
+    
+    Net::IMAP.stub(:new).and_return(@fake_net_imap)
+    
+    @fake_net_imap.stub(:login)
+  end
+  
   context "#initialize" do
-    context "when valid credentials" do
-      before(:each) do
-        @fake_net_imap = double(Net::IMAP)
-        
-        Net::IMAP.stub(:new).and_return(@fake_net_imap)
-        
-        @fake_net_imap.stub(:login)
-      end
-      
-      it "should set imap as Net::IMAP" do
-        Redimap::IMAP.new.imap.should == @fake_net_imap
-      end
-      
-      it "should #close when block" do
-        Redimap::IMAP.new do |imap|
-          imap.should_receive(:close)
-        end
-      end
+    it "should set imap as Net::IMAP" do
+      Redimap::IMAP.new.imap.should == @fake_net_imap
     end
     
-    context "when invalid credentials" do
-      it "should do something or other"
+    it "should #close when block" do
+      Redimap::IMAP.new do |imap|
+        imap.should_receive(:close)
+      end
     end
   end
   
   context "#close" do
-    context "when extant connection" do
-      before(:each) do
-        @fake_net_imap = double(Net::IMAP)
-        
-        Net::IMAP.stub(:new).and_return(@fake_net_imap)
-        
-        @fake_net_imap.stub(:login)
-        
-        @imap = Redimap::IMAP.new
-      end
-      
-      it "should disconnect from IMAP" do
-        @imap.imap.stub(:logout)
-        
-        @imap.imap.should_receive(:disconnect)
-        
-        @imap.close
-      end
-      
-      it "should logout from IMAP" do
-        @imap.imap.stub(:disconnect)
-        
-        @imap.imap.should_receive(:logout)
-        
-        @imap.close
-      end
+    before(:each) do
+      @imap = Redimap::IMAP.new
     end
     
-    context "when inextant connection" do
-      it "should not try to disconnect from IMAP"
+    it "should disconnect from IMAP" do
+      @imap.imap.stub(:logout)
       
-      it "should not try to logout from IMAP"
+      @imap.imap.should_receive(:disconnect)
+      
+      @imap.close
+    end
+    
+    it "should logout from IMAP" do
+      @imap.imap.stub(:disconnect)
+      
+      @imap.imap.should_receive(:logout)
+      
+      @imap.close
     end
   end
   
