867b49c90928bd94bdde6f0aa7321be1e9b8d39d
tiredpixel <tp@tiredpixel.com>
2013-05-18 15:58:50 +0100

    Replace test subject syntax with explicit object.
    Simply because I find it easier to read. ;) And also because it's more
    flexible with those tests requiring extra setup before the 'do' part is
    called.

--- test/redimap/config.rb
+++ test/redimap/config.rb
@@ -6,47 +6,47 @@ require_relative '../../lib/redimap/config'
 describe Redimap::Config do
   
   describe "default" do
-    
-    subject { Redimap::Config.new }
+    before do
+      @config = Redimap::Config.new
+    end
     
     it "sets log_level to INFO" do
-      subject.log_level.must_equal 'INFO'
+      @config.log_level.must_equal 'INFO'
     end
     
     it "sets imap_port to 993" do
-      subject.imap_port.must_equal 993
+      @config.imap_port.must_equal 993
     end
     
     it "sets imap_mailboxes to INBOX" do
-      subject.imap_mailboxes.must_equal ['INBOX']
+      @config.imap_mailboxes.must_equal ['INBOX']
     end
     
     it "sets redis_url to redis://127.0.0.1:6379/0" do
-      subject.redis_url.must_equal 'redis://127.0.0.1:6379/0'
+      @config.redis_url.must_equal 'redis://127.0.0.1:6379/0'
     end
     
     it "sets redis_ns_redimap to redimap" do
-      subject.redis_ns_redimap.must_equal 'redimap'
+      @config.redis_ns_redimap.must_equal 'redimap'
     end
     
     it "sets redis_ns_queue to resque" do
-      subject.redis_ns_queue.must_equal 'resque'
+      @config.redis_ns_queue.must_equal 'resque'
     end
     
     it "sets polling_interval to 60" do
-      subject.polling_interval.must_equal 60
+      @config.polling_interval.must_equal 60
     end
-    
   end
   
   describe "#to_s" do
-    
-    subject { Redimap::Config.new.to_s }
-    
-    it "stringifies hash of config" do
-      subject.must_equal '{:log_level=>"INFO", :imap_host=>nil, :imap_port=>993, :imap_username=>nil, :imap_mailboxes=>["INBOX"], :redis_url=>"redis://127.0.0.1:6379/0", :redis_ns_redimap=>"redimap", :redis_ns_queue=>"resque", :polling_interval=>60}'
+    before do
+      @config = Redimap::Config.new
     end
     
+    it "stringifies hash of config" do
+      @config.to_s.must_equal '{:log_level=>"INFO", :imap_host=>nil, :imap_port=>993, :imap_username=>nil, :imap_mailboxes=>["INBOX"], :redis_url=>"redis://127.0.0.1:6379/0", :redis_ns_redimap=>"redimap", :redis_ns_queue=>"resque", :polling_interval=>60}'
+    end
   end
   
 end
